c0abf5c476f6f5d23d4c35e2cc2e7aeb24ffb86d
Toms Mikoss <toms.mikoss@gmail.com>
2020-12-20 00:42:14 +0200

    Basic data api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,18 +10,31 @@ const app = express()
 
 app.set('views', 'public')
 
-const dataQuery = `
-  SELECT a.name,
-         m."totalCases",
-         m.date
-  FROM metrics m
-  JOIN areas a ON m."areaId" = a.id
-  WHERE m.date = (SELECT max(date) FROM metrics)
-`
+app.get('/api/countries', async (_req, res) => {
+  const [data] = await database.query(`
+    SELECT a.id,
+           a.name,
+           ST_AsGeoJSON(a.geography, 4) geography
+    FROM areas a
+    WHERE a.kind = 'country'
+  `)
+  res.json(data)
+})
+
+app.get('/api/totalcases', async (_req, res) => {
+  const [data] = await database.query(`
+    SELECT a.id AS countryId,
+           COALESCE(JSON_OBJECT_AGG(m.date, m."totalCases"), '{}' ) AS totalCases
+    FROM metrics m
+    JOIN areas a ON m."areaId" = a.id
+    WHERE a.kind = 'country'
+    GROUP BY a.id
+  `)
+  res.json(data)
+})
 
 app.get('/*', async (_req, res) => {
-  const [data] = await database.query(dataQuery)
-  res.json(data)
+  res.json({ hello: 'world' })
 })
 
 const server = http.createServer(app)
