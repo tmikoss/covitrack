3f94de40d968584fd3d096b838563b4f49ee79c5
Toms Mikoss <toms.mikoss@gmail.com>
2020-12-20 21:31:28 +0200

    Fix warnings

--- frontend/src/hooks/cases.ts
+++ frontend/src/hooks/cases.ts
@@ -42,5 +42,5 @@ export const useCaseData = () => {
     if (!loaded) {
       load()
     }
-  }, [loaded])
+  }, [load, loaded])
 }
--- frontend/src/hooks/countries.ts
+++ frontend/src/hooks/countries.ts
@@ -84,5 +84,5 @@ export const useCountryData = () => {
     if (!loaded) {
       load()
     }
-  }, [loaded])
+  }, [load, loaded])
 }
