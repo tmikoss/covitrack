f9339fda6442de7a5936cbeb7e06a50a45bed08c
Toms Mikoss <toms.mikoss@gmail.com>
2020-12-27 20:32:01 +0200

    Cache cases endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,15 @@ import compression from 'compression'
 import { join } from 'path'
 import { database } from './db'
 import { scheduleRegularUpdates } from './owidLoader'
+import { redis } from './redis'
+import cacheManager from 'cache-manager'
+import cacheManagerIoredis from 'cache-manager-ioredis'
+
+const cache = cacheManager.caching({
+  store: cacheManagerIoredis,
+  redisInstance: redis,
+  ttl: 600
+})
 
 const app = express()
 
@@ -13,15 +22,20 @@ app.use(compression())
 app.use(express.static(join(__dirname, '../public'), { index: 'index.html' }))
 
 app.get('/api/cases.json', async (_req, res) => {
-  const [data] = await database.query(`
-    SELECT a.code AS country,
-           COALESCE(JSON_OBJECT_AGG(TO_CHAR(m.date, 'YYYYMMDD'), m."newCasesPerMillionSmoothed"), '{}' ) AS "newCases",
-           COALESCE(JSON_OBJECT_AGG(TO_CHAR(m.date, 'YYYYMMDD'), m."newDeathsPerMillionSmoothed"), '{}' ) AS "newDeaths"
-    FROM metrics m
-    JOIN areas a ON m."areaId" = a.id
-    GROUP BY a.id
-    ORDER BY a.code
-  `)
+  const data = await cache.wrap('/api/cases.json', async (cb: (result: any) => void) => {
+    const [data] = await database.query(`
+      SELECT a.code AS country,
+            COALESCE(JSON_OBJECT_AGG(TO_CHAR(m.date, 'YYYYMMDD'), m."newCasesPerMillionSmoothed"), '{}' ) AS "newCases",
+            COALESCE(JSON_OBJECT_AGG(TO_CHAR(m.date, 'YYYYMMDD'), m."newDeathsPerMillionSmoothed"), '{}' ) AS "newDeaths"
+      FROM metrics m
+      JOIN areas a ON m."areaId" = a.id
+      GROUP BY a.id
+      ORDER BY a.code
+    `)
+
+    cb(data)
+  })
+
   res.json(data)
 })
 
